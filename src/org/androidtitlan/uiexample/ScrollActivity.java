945b9790efe8f8e499c00596415b384905612b0b
Enrique Diaz <nrikediaz@gmail.com>
2010-11-18 00:56:12 -0600

    Hello Layouts Example to #shlcon2010

---
+++ src/org/androidtitlan/uiexample/ScrollActivity.java
@@ -0,0 +1,16 @@
+package org.androidtitlan.uiexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScrollActivity extends Activity {
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scrolllayout);
+	}
+
+}
